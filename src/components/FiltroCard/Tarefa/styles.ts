4faf3297fa2eb274c6c91e562d3fad57d0137d60
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-25 14:21:46 -0300

    estilização de botões e tags

--- src/components/FiltroCard/Tarefa/styles.ts
+++ src/components/FiltroCard/Tarefa/styles.ts
@@ -1,4 +1,26 @@
 import styled from 'styled-components'
+import variaveis from '../../../styles/variaveis'
+
+import * as enums from '../../../utils/enums/Tarefa'
+
+type TagProps = {
+  prioridade?: enums.Prioridade
+  status?: enums.Status
+  parametro: 'status' | 'prioridade'
+}
+
+function retornaCorFundo(props: TagProps): string {
+  if (props.parametro === 'status') {
+    if (props.status === enums.Status.PENDENTE) return variaveis.amarelo
+    if (props.status === enums.Status.CONCLUIDA) return variaveis.verde
+  } else {
+    if (props.prioridade === enums.Prioridade.URGENTE) return variaveis.vermelho
+    if (props.prioridade === enums.Prioridade.IMPORTANTE)
+      return variaveis.amarelo2
+  }
+
+  return '#ccc'
+}
 
 export const Card = styled.div`
   padding: 16px;
@@ -14,12 +36,12 @@ export const Titulo = styled.h3`
   margin-bottom: 16px;
 `
 
-export const Tag = styled.span`
+export const Tag = styled.span<TagProps>`
   padding: 4px 8px;
   color: #fff;
   font-size: 10px;
   font-weight: bold;
-  background-color: #e1a32a;
+  background-color: ${(props) => retornaCorFundo(props)};
   border-radius: 8px;
   margin-right: 16px;
   display: inline-block;
@@ -54,4 +76,10 @@ export const Botao = styled.button`
   background-color: #2f3640;
   border-radius: 8px;
   margin-right: 8px;
+`
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.verde};
+`
+export const BotaoCancelarRemover = styled(Botao)`
+  background-color: ${variaveis.vermelho};
 `
--- src/containers/BarraLateral/ListaDeTarefas/index.tsx
+++ src/containers/BarraLateral/ListaDeTarefas/index.tsx
@@ -1,25 +1,43 @@
 import Tarefa from '../../../components/FiltroCard/Tarefa'
 import { Container } from './styles'
 
+import * as enums from '../../../utils/enums/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Estudar React',
+    descricao: 'Ver a aula 1 da EBAC',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'Fazer treino B',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: categoria e termo</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((trf) => (
+        <li key={trf.titulo}>
+          <Tarefa
+            descricao={trf.descricao}
+            titulo={trf.titulo}
+            prioridade={trf.prioridade}
+            status={trf.status}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
