61fdf7835dec0a88b3f5f72b262256b7633a97a0
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-25 15:33:54 -0300

    implementação do redux

--- src/containers/BarraLateral/ListaDeTarefas/index.tsx
+++ src/containers/BarraLateral/ListaDeTarefas/index.tsx
@@ -1,45 +1,30 @@
-import Tarefa from '../../../components/FiltroCard/Tarefa'
+import { useSelector } from 'react-redux'
+
 import { Container } from './styles'
+import Tarefa from '../../../components/FiltroCard/Tarefa'
 
-import * as enums from '../../../utils/enums/Tarefa'
+import { RootReducer } from '../../../store'
 
-const tarefas = [
-  {
-    titulo: 'Estudar TypeScript',
-    descricao: 'Ver a aula 3 da EBAC',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  },
-  {
-    titulo: 'Estudar React',
-    descricao: 'Ver a aula 1 da EBAC',
-    prioridade: enums.Prioridade.URGENTE,
-    status: enums.Status.CONCLUIDA
-  },
-  {
-    titulo: 'Ir para a academia',
-    descricao: 'Fazer treino B',
-    prioridade: enums.Prioridade.IMPORTANTE,
-    status: enums.Status.PENDENTE
-  }
-]
+const ListaDeTarefas = () => {
+  const { tarefas } = useSelector((state: RootReducer) => state)
 
-const ListaDeTarefas = () => (
-  <Container>
-    <p>2 tarefas marcadas como: categoria e termo</p>
-    <ul>
-      {tarefas.map((trf) => (
-        <li key={trf.titulo}>
-          <Tarefa
-            descricao={trf.descricao}
-            titulo={trf.titulo}
-            prioridade={trf.prioridade}
-            status={trf.status}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-)
+  return (
+    <Container>
+      <p>2 tarefas marcadas como: categoria e termo</p>
+      <ul>
+        {tarefas.map((trf) => (
+          <li key={trf.titulo}>
+            <Tarefa
+              descricao={trf.descricao}
+              titulo={trf.titulo}
+              prioridade={trf.prioridade}
+              status={trf.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
