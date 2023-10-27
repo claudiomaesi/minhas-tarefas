50d3a0aa3a73fa65bc9a09166e116cf94f247e6f
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-27 15:23:40 -0300

    implementação dos filtros

--- src/containers/BarraLateral/ListaDeTarefas/index.tsx
+++ src/containers/BarraLateral/ListaDeTarefas/index.tsx
@@ -7,12 +7,42 @@ import { RootReducer } from '../../../store'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo !== undefined) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
 
   return (
     <Container>
-      <p>2 tarefas marcadas como: categoria e termo</p>
+      <p>2 tarefas marcadas como: categoria e {termo}</p>
       <ul>
-        {itens.map((trf) => (
+        <li>{termo}</li>
+        <li>{criterio}</li>
+        <li>{valor}</li>
+      </ul>
+      <ul>
+        {filtraTarefas().map((trf) => (
           <li key={trf.titulo}>
             <Tarefa
               id={trf.id}
